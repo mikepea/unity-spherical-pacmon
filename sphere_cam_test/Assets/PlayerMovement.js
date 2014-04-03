fc092502157ceb24977142aa51a3b50736d9b93d
Mike Pountney <Mike.Pountney@gmail.com>
2014-04-03 11:27:15 +0100

    init

---
+++ sphere_cam_test/Assets/PlayerMovement.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var speed = 5.0;
+
+function Start () {
+
+}
+
+function Update () {
+  var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+  var y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+  transform.Translate(x, y, 0);
+}
---
+++ sphere_cam_test/Assets/RotateWorld.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+  transform.RotateAround(Vector3.zero, Vector3.up, -20 * Time.deltaTime);
+}

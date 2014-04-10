932fc060a60c96012f5671bd5b36e64d122881c2
Mike Pountney <Mike.Pountney@gmail.com>
2014-04-10 23:28:40 +0100

    add Globals

---
+++ sphere_cam_test/Assets/GlobalGameDetails.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalGameDetails : MonoBehaviour
+{
+
+    public static float maxAngleY = 80F;
+    public static float minAngleY = -80F;
+    public static float cellSpacing = 5F;
+
+
+}

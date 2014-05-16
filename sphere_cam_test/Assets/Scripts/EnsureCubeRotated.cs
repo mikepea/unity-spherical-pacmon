9587d54ec06d52364926094e27fe9c122eee929d
Mike Pountney <Mike.Pountney@gmail.com>
2014-05-16 19:03:12 +0100

    Use a cube pill, less facets :)

---
+++ sphere_cam_test/Assets/Scripts/EnsureCubeRotated.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnsureCubeRotated : MonoBehaviour
+{
+    
+    void Start ()
+    {
+        transform.LookAt (Vector3.zero);
+    }
+    
+}

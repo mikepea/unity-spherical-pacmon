43f7516a81e7ebcb11a129ed704ddfecc32a0c91
Mike Pountney <Mike.Pountney@gmail.com>
2014-08-17 23:19:07 +0100

    Allow for projectors to be mounted above/below equator
    By positioning the cameras above the equator, we allow
    the projectors to be mounted higher, which reduces shadows.
    
    We lose some resolution at the bottom of course (and gain it at the top)

---
+++ sphere_cam_test/Assets/Scripts/LookAtOrigin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtOrigin : MonoBehaviour
+{
+
+    public float rotX, rotY, rotZ;
+
+    void Start ()
+    {
+        transform.LookAt (Vector3.zero);
+        transform.Rotate (rotX, rotY, rotZ);
+    }
+	
+}

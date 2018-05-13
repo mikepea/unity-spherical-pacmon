19c02bdb18e75310b98f33690ffc0513551f49d0
Mike Pountney <Mike.Pountney@gmail.com>
2018-05-13 07:05:46 -0700

    Add top/bottom side blinds. More Global vars. Also mono reformat :/
    Top and Side blinds cut out some of the bleed at the top/bottom of the
    sphere.
    
    Mono has trampled over the code again, so yey lots of whitespace change
    
    I think there are some more globals affecting things like camera FOV too

--- sphere_cam_test/Assets/Scripts/GlobalGameDetails.cs
+++ sphere_cam_test/Assets/Scripts/GlobalGameDetails.cs
@@ -12,7 +12,20 @@ public class GlobalGameDetails : MonoBehaviour
     public static GlobalGameDetails i;
 
     public float blindOffset;
+    public float blindTopX;
+    public float blindTopY;
+    public float blindTopRot;
+    public float blindBottomX;
+    public float blindBottomY;
+    public float blindBottomRot;
+    public float cameraFieldOfView;
+    public float cameraYOffset;
+    public float cameraDistanceFromOrigin;
+
+    public float sphereSize;
+
     public bool displayTestPattern = false;
+    public bool displayTestMap = false;
 
     public int initialMapNumber;
     public int numberOfMaps;
@@ -36,181 +49,272 @@ public class GlobalGameDetails : MonoBehaviour
     private int score;
     private int highScore;
 
-    public void Start() {
+    private GameObject mainSphere;
 
-        if ( disableAudio ) {
-          DisableAudio();
+    public void Start ()
+    {
+
+        if (disableAudio) {
+            DisableAudio ();
         }
 
-        if ( gameMode == "Init" ) {
-          ZeroScore();
-          ResetMapNumber();
-          StartGameDemo();
+        if (gameMode == "Init") {
+            ZeroScore ();
+            ResetMapNumber ();
+            StartGameDemo ();
         }
 
     }
 
-    public void ResetMapNumber() {
-      if ( displayTestPattern ) {
-        mapNumber = 0;
-      } else {
-        mapNumber = initialMapNumber;
-      }
+    public void ResetMapNumber ()
+    {
+        if (displayTestMap) {
+            mapNumber = 0;
+        } else {
+            mapNumber = initialMapNumber;
+        }
     }
 
-    public float BlindOffset() {
+    public float CameraYOffset ()
+    {
+        return cameraYOffset;
+    }
+
+    public float CameraDistanceFromOrigin ()
+    {
+        return cameraDistanceFromOrigin;
+    }
+
+    public float CameraFieldOfView ()
+    {
+        return cameraFieldOfView;
+    }
+
+    public float BlindOffset ()
+    {
         return blindOffset;
     }
 
-    public float LevelStartTime() {
-      return levelStartTime;
+    public float BlindTopX ()
+    {
+        return blindTopX;
     }
 
-    public bool MovementEnabled() {
-      return movementEnabled;
+    public float BlindTopY ()
+    {
+        return blindTopY;
     }
 
-    public bool InDemoMode() {
-      Debug.Log ( "InDemoMode: " + demoMode );
-      return demoMode;
+    public float BlindTopRot ()
+    {
+        return blindTopRot;
     }
 
-    public void EnableMovement() {
-      movementEnabled = true;
+    public float BlindBottomX ()
+    {
+        return blindBottomX;
     }
 
-    public void DisableMovement() {
-      movementEnabled = false;
+    public float BlindBottomY ()
+    {
+        return blindBottomY;
     }
 
-    public void ZeroScore() {
-      score = 0;
+    public float BlindBottomRot ()
+    {
+        return blindBottomRot;
     }
 
-    public void IncreaseScore(int increment) {
-      if ( ! InDemoMode() ) score += increment;
+    public float SphereSize ()
+    {
+        return sphereSize;
     }
 
-    public int Score() {
+    public float LevelStartTime ()
+    {
+        return levelStartTime;
+    }
+
+    public bool MovementEnabled ()
+    {
+        return movementEnabled;
+    }
+
+    public bool InDemoMode ()
+    {
+        Debug.Log ("InDemoMode: " + demoMode);
+        return demoMode;
+    }
+
+    public void EnableMovement ()
+    {
+        movementEnabled = true;
+    }
+
+    public void DisableMovement ()
+    {
+        movementEnabled = false;
+    }
+
+    public void ZeroScore ()
+    {
+        score = 0;
+    }
+
+    public void IncreaseScore (int increment)
+    {
+        if (! InDemoMode ())
+            score += increment;
+    }
+
+    public int Score ()
+    {
         return score;
     }
 
-    public int HighScore() {
+    public int HighScore ()
+    {
         return highScore;
     }
 
-    public void SetHighScore(int score) {
-      if ( ! InDemoMode() ) highScore = score;
+    public void SetHighScore (int score)
+    {
+        if (! InDemoMode ())
+            highScore = score;
     }
 
-    public string MapName() {
+    public string MapName ()
+    {
         return "map" + mapNumber;
     }
 
-    public void NextMap() {
-      if ( displayTestPattern ) {
-        mapNumber = 0;
-      } else {
-        mapNumber++;
-        mapNumber = mapNumber % numberOfMaps;
-        if ( mapNumber == 0 )  mapNumber++; //  always skip map0 -- test pattern
-      }
-      DisableMovement();
-      Debug.Log("MIKEDEBUG: (in NextMap) " + GameMode() );
-      Application.LoadLevel(0);
+    public void NextMap ()
+    {
+        if (displayTestMap) {
+            mapNumber = 0;
+        } else {
+            mapNumber++;
+            mapNumber = mapNumber % numberOfMaps;
+            if (mapNumber == 0)
+                mapNumber++; //  always skip map0 -- test pattern
+        }
+        DisableMovement ();
+        Debug.Log ("MIKEDEBUG: (in NextMap) " + GameMode ());
+        Application.LoadLevel (0);
     }
 
-    public bool AudioEnabled() {
+    public bool AudioEnabled ()
+    {
         return audioEnabled;
     }
 
-    public void DisableAudio() {
+    public void DisableAudio ()
+    {
         audioEnabled = false;
     }
 
-    public void EnableAudio() {
-      if ( ! disableAudio ) {
-        audioEnabled = true;
-      }
+    public void EnableAudio ()
+    {
+        if (! disableAudio) {
+            audioEnabled = true;
+        }
     }
 
-    public string GameMode() {
+    public string GameMode ()
+    {
         return gameMode;
     }
 
-    public float GameModeStartTime() {
+    public float GameModeStartTime ()
+    {
         return gameModeStartTime;
     }
 
-    public void Awake() {
-      if ( !i ) {
-        i = this;
-        DontDestroyOnLoad(this);
-      } else {
-        Destroy(gameObject);
-      }
-    }
-
-    public void GameStart() {
-      gameMode = "GameStart";
-      demoMode = false;
-      ZeroScore();
-      EnableAudio();
-      ResetMapNumber();
-      Application.LoadLevel(0);
-    }
-
-    public void StartLevel() {
-      gameMode = "StartLevel";
-      levelStartTime = Time.time;
-      gameModeStartTime = Time.time;
-      DisableMovement();
-    }
-
-    public void GameOver() {
-      if ( gameMode != "GameOver" ) {
-        gameMode = "GameOver";
-        gameModeStartTime = Time.time;
-        DisableAudio();
-      }
-    }
-
-    public void StartGameDemo() {
-      demoMode = true;
-      gameMode = "GameStart";
-      DisableAudio();
-      ResetMapNumber();
-      Application.LoadLevel(0);
-    }
-
-    public void GameInProgress() {
-      gameModeStartTime = Time.time;
-      gameMode = "GameInProgress";
-    }
-
-    public void FixedUpdate() {
-      Debug.Log("MIKEDEBUG: " + GameMode() );
-      if (Input.GetKey (nextGameModeKey)) {
-        Debug.Log("nextGameModeKeyPressed: " + gameMode);
-        if ( demoMode ) {
-          demoMode = false;
-          GameStart();
+    public void Awake ()
+    {
+        if (!i) {
+            i = this;
+            DontDestroyOnLoad (this);
         } else {
-          StartGameDemo();
+            Destroy (gameObject);
         }
-      }
+    }
 
-      if (Input.GetKey (nextLevelKey)) {
-        Debug.Log("nextLevelKeyPressed");
-        NextMap();
-      }
+    public void GameStart ()
+    {
+        gameMode = "GameStart";
+        demoMode = false;
+        ZeroScore ();
+        EnableAudio ();
+        ResetMapNumber ();
+        Application.LoadLevel (0);
+    }
 
-      Debug.Log("GameMode " + GameMode() + " start time " + GameModeStartTime());
-      if ( GameMode() == "GameOver" && 
-           GameModeStartTime() + demoStartDelay < Time.time
+    public void StartLevel ()
+    {
+        gameMode = "StartLevel";
+        levelStartTime = Time.time;
+        gameModeStartTime = Time.time;
+        DisableMovement ();
+    }
+
+    public void GameOver ()
+    {
+        if (gameMode != "GameOver") {
+            gameMode = "GameOver";
+            gameModeStartTime = Time.time;
+            DisableAudio ();
+        }
+    }
+
+    public void StartGameDemo ()
+    {
+        demoMode = true;
+        gameMode = "GameStart";
+        DisableAudio ();
+        ResetMapNumber ();
+        Application.LoadLevel (0);
+    }
+
+    public void GameInProgress ()
+    {
+        gameModeStartTime = Time.time;
+        gameMode = "GameInProgress";
+    }
+
+    public void FixedUpdate ()
+    {
+
+        if (displayTestPattern) {
+            GameObject[] spheres = GameObject.FindGameObjectsWithTag ("MainSphere");
+            mainSphere = spheres[0];
+            mainSphere.SetActive(false);
+            // This will lock the game, and a stop/play will be needed to
+            // restart :(
+        }
+
+        Debug.Log ("MIKEDEBUG: " + GameMode ());
+        if (Input.GetKey (nextGameModeKey)) {
+            Debug.Log ("nextGameModeKeyPressed: " + gameMode);
+            if (demoMode) {
+                demoMode = false;
+                GameStart ();
+            } else {
+                StartGameDemo ();
+            }
+        }
+
+        if (Input.GetKey (nextLevelKey)) {
+            Debug.Log ("nextLevelKeyPressed");
+            NextMap ();
+        }
+
+        Debug.Log ("GameMode " + GameMode () + " start time " + GameModeStartTime ());
+        if (GameMode () == "GameOver" && 
+            GameModeStartTime () + demoStartDelay < Time.time
            ) {
-        StartGameDemo();
-      }
+            StartGameDemo ();
+        }
     }
 
 }
